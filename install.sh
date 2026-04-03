c6b803f804c7e9bd4e46ad3e6347fd55ac8d2dea
Bilko <StanislavBG@gmail.com>
2026-04-02 23:39:48 -0700

    Rename requirements.txt to deps.txt — Replit auto-runs pip on requirements.txt

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf .pythonlibs
-python3 -m pip install --target=.pythonlibs --no-cache-dir -r requirements.txt 2>&1 | grep -v TypeError
+python3 -m pip install --target=.pythonlibs --no-cache-dir -r deps.txt 2>&1 | grep -v TypeError
 exit 0
